4ba582d0c026a41ca206baf23bb20c2b29db318b
hemnys <hemnys25@gmail.com>
2024-12-10 07:33:34 -0400

    Rearranged all files into folders for eeach component

--- src/App/index.js
+++ src/App/index.js
@@ -1,34 +1,38 @@
 import React from "react";
-import { CreateTodoButton } from "./CreateTodoButton";
-import { TodoCounter } from "./TodoCounter";
-import { TodoItem } from "./TodoItem";
-import { TodoList } from "./TodoList";
-import { TodoSearch } from "./TodoSearch";
+import { CreateTodoButton } from "../CreateTodoButton";
+import { TodoCounter } from "../TodoCounter";
+import { TodoItem } from "../TodoItem";
+import { TodoList } from "../TodoList";
+import { TodoSearch } from "../TodoSearch";
 
-// const defaultTodos = [
-//   { text: "Cortar cebolla", completed: true },
-//   { text: "Tomar el curso de intro a React", completed: false },
-//   { text: "Llorar con la llorona", completed: false },
-//   { text: "Usar estados derivado", completed: true },
-// ];
-// localStorage.setItem("TODOS_V1", JSON.stringify(defaultTodos));
+
+
+function useLocalStorage(itemName, initialValue) {
+
+  const localStorageItem = localStorage.getItem(itemName);
+  const parsedItem = (!localStorageItem) ? initialValue : JSON.parse(localStorageItem); 
+  const [item, setItem] = React.useState(parsedItem);
+  const saveItem = (newItem) => {
+    const stringifiedItem = JSON.stringify(newItem);
+    localStorage.setItem(itemName,stringifiedItem );
+    setItem(newItem);
+  }
+  return [
+    item,
+    saveItem,
+  ];
+}
 
 function App() {
 
-  const localStorageTodos = localStorage.getItem("TODOS_V1") || "[]";
-  let parsedTodos = JSON.parse(localStorageTodos);
+  const [todos, saveTodos] = useLocalStorage("TODOS_V1", []);
   const [searchValue, setSearchValue] = React.useState("");
-  const [todos, setTodos] = React.useState(parsedTodos);
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
   const totalTodos = todos.length;
   const filteredTodos = todos.filter((todo) =>
     todo.text.toLowerCase().includes(searchValue.toLowerCase())
   );
-  const saveTodos = (newTodos) => {
-    const stringifiedTodos = JSON.stringify(newTodos);
-    localStorage.setItem("TODOS_V1", stringifiedTodos);
-    setTodos(newTodos);
-  }
+  
   const completeTodo = (text) => {
     const newTodos = [...todos];
     const todoIndex = newTodos.findIndex((todo) => todo.text === text);
